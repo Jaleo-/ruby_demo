1014b93c6db4f79f0136547d68b3c3073f6122f8
Desmond <desmondD@gmx.com>
2014-03-05 16:20:56 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstFuckityFuck::Application.config.secret_token = 'd75fb553be8fcf16c27be576f91b390fd50590d501b2b4b490f4ec7d220ac02fc36dbe0687cf53e7aa1e81df63fdeb7e4f789bd86e595a298a95c71cff70fd52'
